27f62aa08af035e7726c779a7bd942f6bf465497
kyle <kyle@solidstategroup.com>
2019-08-20 10:41:23 +0100

    revert http2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // This file doesn't go through babel or webpack transformation.
 // Make sure the syntax and sources this file requires are compatible with the current node version you are running
 // See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
-const { createServer } = require('http2');
+const { createServer } = require('http');
 const { parse } = require('url');
 const { join } = require('path');
 const next = require('next');
