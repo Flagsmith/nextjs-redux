3b89d9f24aa17d0963dd698b732c1e7f7f80302f
kyle <kyle@solidstategroup.com>
2019-08-20 12:03:38 +0100

    test

--- web/pages/about.js
+++ web/pages/about.js
@@ -10,6 +10,7 @@ class AboutPage extends PureComponent {
             <div className="container">
                 <h1>Home</h1>
                 About page
+                <img src="https://as1.ftcdn.net/jpg/00/92/53/56/500_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg"/>
             </div>
         );
     }
