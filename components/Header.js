a2ab737111890b0bdcdfb7664e29db96eeb9b998
kyle <kyle@solidstategroup.com>
2019-08-24 16:17:53 +0100

    style changes

--- components/Header.js
+++ components/Header.js
@@ -1,33 +1,47 @@
 import Link from 'next/link';
 import withAuth from '../common/providers/withAuth';
 
-const linkStyle = {
-    marginRight: 15,
-};
-
 function Header(props) {
     return (
-        <div>
-            <Link href="/">
-                <a style={linkStyle}>Home</a>
-            </Link>
-            <Link href="/about">
-                <a style={linkStyle}>About</a>
-            </Link>
-            <Link href="/privacy">
-                <a style={linkStyle}>Privacy</a>
-            </Link>
-            <Link href="/markup">
-                <a style={linkStyle}>Markup</a>
-            </Link>
-            {props.user ? (
-                <a onClick={props.logout} style={linkStyle}>Logout</a>
-            ) : (
-                <Link href="/login">
-                    <a style={linkStyle}>Login</a>
-                </Link>
-            )}
-        </div>
+        <nav className="navbar navbar-fixed-top navbar-light">
+            <ul className="nav justify-content-start">
+                <li className="nav-item">
+                    <Link href="/" className="nav-link">
+                        <a className="nav-link">Home</a>
+                    </Link>
+                </li>
+                {/* Examples */}
+                <React.Fragment>
+                    <li className="nav-item">
+                        <Link href="/about">
+                            <a className="nav-link">About</a>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/privacy">
+                            <a className="nav-link">About</a>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/privacy">
+                            <a className="nav-link">Privacy</a>
+                        </Link>
+                    </li>
+                </React.Fragment>
+                {/* End of Examples */}
+            </ul>
+            <ul className="nav justify-content-end">
+                <li className="nav-item">
+                    {props.user ? (
+                        <a className="nav-link" onClick={props.logout}>Logout</a>
+                    ) : (
+                        <Link href="/login">
+                            <a className="nav-link">Login</a>
+                        </Link>
+                    )}
+                </li>
+            </ul>
+        </nav>
     );
 }
 export default withAuth(Header);
